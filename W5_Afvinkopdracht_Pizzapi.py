4f8e2b5d83b9a47188e3d1594184970faddb7b92
kimbredenkamp <43311756+kimbredenkamp@users.noreply.github.com>
2020-12-11 11:49:28 +0100

    Add files via upload

--- W5_Afvinkopdracht_Pizzapi.py
+++ W5_Afvinkopdracht_Pizzapi.py
@@ -3,19 +3,28 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 pizzaDB = [
-    {'name': 'vegetariana'},  # Joost, Paul en Kim
-    {'name': 'quattro formaggi'},  # Koen
-    {'name': 'taco'},
-    {'name': 'pizza pesto'},
-    {'name': 'siliciana'},
-    {'name': 'quattro stagioni'},
-    {'name': 'funghi'},  # Speciaal voor Ruud;)
+    {'name': 'vegetariana', 'price': 8.75},  # Joost, Paul
+    {'name': 'quattro formaggi', 'price': 9},  # Koen
+    {'name': 'taco', 'price': 9},  # Sanne
+    {'name': 'pizza pesto', 'price': 10},  # Kim
+    {'name': 'siliciana', 'price': 8.5},  # Ruud
+    {'name': 'quattro stagioni', 'price': 8.75},
+    {'name': 'funghi', 'price': 7.5},  # Speciaal voor Ruud;)
             ]
 
 
 @app.route("/", methods=['GET'])
 def getPizza():
     return jsonify({'pizzaDB': pizzaDB})
+
+
+@app.route("/<string:name>", methods=['GET'])
+def getOnePizza(name):
+    resultPizza = []
+    for pizza in pizzaDB:
+        if pizza['name'] == name:
+            resultPizza.append(pizza)
+    return jsonify({'pizzaDB': resultPizza})
 
 
 if __name__ == "__main__":
